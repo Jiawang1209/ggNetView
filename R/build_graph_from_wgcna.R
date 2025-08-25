25d9c85960a03118c0279cbd421079a1f0a8a444
yueliu1115 <yueliu1115@163.com>
2025-08-25 16:19:38 +0800

    add build_graph_from_wgcnaw function

---
+++ R/build_graph_from_wgcna.R
@@ -0,0 +1,8 @@
+build_graph_from_wgcna <- function(df,
+                                   node_annotation = NULL,
+                                   directed = F,
+                                   method = "cluster_fast_greedy",
+                                   top_modules = 15,
+                                   seed = 1115){
+
+}
