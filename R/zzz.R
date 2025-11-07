b3829216cec2899a3823986b8688615fbd55fcf2
Jiawang1209 <Jiawang1209@163.com>
2025-11-07 10:53:45 +0800

    update

---
+++ R/zzz.R
@@ -0,0 +1,29 @@
+.onAttach <- function(libname, pakname){
+  packageStartupMessage(
+    "
+                                               ░██               ░██
+                                               ░██
+ ░████████  ░████████ ░████████   ░███████  ░████████ ░██    ░██ ░██ ░███████  ░██    ░██    ░██
+░██    ░██ ░██    ░██ ░██    ░██ ░██    ░██    ░██    ░██    ░██ ░██░██    ░██ ░██    ░██    ░██
+░██    ░██ ░██    ░██ ░██    ░██ ░█████████    ░██     ░██  ░██  ░██░█████████  ░██  ░████  ░██
+░██   ░███ ░██   ░███ ░██    ░██ ░██           ░██      ░██░██   ░██░██          ░██░██ ░██░██
+ ░█████░██  ░█████░██ ░██    ░██  ░███████      ░████    ░███    ░██ ░███████     ░███   ░███
+       ░██        ░██
+ ░███████   ░███████
+
+
+  ggNetView v0.1.0 (2025)
+  Developed by Jiawang's Network Visualization Group
+
+  Maintainers:
+   - Jiawang Liu <Jiawang1209@163.com>
+   - Co-maintainer <someone@example.com>
+
+  GitHub: https://github.com/Jiawang1209/ggNetView
+  Bug Reports: https://github.com/Jiawang1209/ggNetView/issues
+
+  Type citation('ggNetView') for how to cite this package.
+  Run browseVignettes('ggNetView') for documentation.
+                        "
+  )
+}
